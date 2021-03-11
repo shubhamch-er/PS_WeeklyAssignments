3722088b99a6175656e9be91ea8927a784852bde
shubhamch-er <shubhamch.er@gmail.com>
2021-03-11 15:35:36 +0530

    Working on Reviews

--- week1/gitAssignment/src/main/java/HelloWorkd.java
+++ week1/gitAssignment/src/main/java/HelloWorkd.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 		System.out.println ("Hello World from Shubham Chaudhary");
-		System.out.println("Working with maven");
+		System.out.println("Working with Maven");
     }
 }
