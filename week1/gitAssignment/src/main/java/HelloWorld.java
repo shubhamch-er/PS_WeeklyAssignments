d4e9a2f78e67097ae5d668ef4c7808f47d53b232
Sachin Choudhary <sachinbarkhera1999@gmail.com>
2021-03-12 16:15:16 +0530

    changes by sachin

--- week1/gitAssignment/src/main/java/HelloWorld.java
+++ week1/gitAssignment/src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-		System.out.println ("Hello World from Shubham Chaudhary");
+		System.out.println ("Hello World from Sachin Choudhary");
 		System.out.println("Working with Maven");
 		System.out.println("Changes made in VSCode");
     }
